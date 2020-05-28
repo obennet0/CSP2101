1f15d56491633ea0864d494b4d84332e1f4f7697
Owen Bennett <owen@ubuntu.c137>
2020-05-28 20:47:26 +0800

    adjustment and rename of connection script

--- assignment/connect.sh
+++ assignment/connect.sh
@@ -2,6 +2,6 @@
 
 curl -s "https://www.ecu.edu.au/service-centres/MACSC/gallery/gallery.php?folder=152" > temp.txt
 
-cat temp.txt | grep "jpg" | sed -e 's/<img src="//' -e 's/".*//' -e 's/^[ \t]*//' > urls.txt
-rm temp.txt
+cat temp.txt | grep "jpg" | sed -e 's/<img src="//' -e 's/".*//' -e 's/^[ \t]*//' > url.txt
 
+rm temp.txt
