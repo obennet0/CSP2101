82ea9b61406d5d8fa3fcc542cce590a3ccea727e
obennet0 <obennet0@our.ecu.edu.au>
2020-05-28 12:36:37 +0800

    minor adjustments to all.sh script

--- assignment/all.sh
+++ assignment/all.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-if [ -d all_images ]; then
-    rm -r all_images
+if [ -d images ]; then
+    rm -r images
 fi
 
-mkdir all_images/
+mkdir images
+echo "Downloading images into 'images' folder:"
+echo
+sleep 2
 file=url.txt
 
 for url in $(cat $file); do
-    wget -q $url -P all_images/
+    wget -q $url -P images/
     file_name=$(echo $url | awk -F / '{print $8}')
     image_num=$(echo $url | awk -F / '{print $8}' | sed 's/.jpg//')
-    file_size=$(du -b all_images/$file_name | awk '{printf "%3.2f", $1/1000}')
+    file_size=$(du -b images/$file_name | awk '{printf "%3.2f", $1/1000}')
     unit=KB
     echo "Downloading $image_num, with the file name $file_name, with a file size of $file_size $unit...File Download Complete"
 done
