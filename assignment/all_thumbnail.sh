c3f8c41f8ef6ae01133e00f744c7fa98c4a516d0
obennet0 <obennet0@our.ecu.edu.au>
2020-05-28 12:22:30 +0800

    complete download ALL component

--- assignment/all_thumbnail.sh
+++ assignment/all_thumbnail.sh
@@ -5,7 +5,16 @@ if [ -d all_images ]; then
 fi
 
 mkdir all_images/
+file=url.txt
 
-wget -i url.txt -P all_images -q
+for url in $(cat $file); do
+    wget -q $url -P all_images/
+    file_name=$(echo $url | awk -F / '{print $8}')
+    image_num=$(echo $url | awk -F / '{print $8}' | sed 's/.jpg//')
+    file_size=$(du -b all_images/$file_name | awk '{printf "%3.2f", $1/1000}')
+    unit=KB
+    echo "Downloading $image_num, with the file name $file_name, with a file size of $file_size $unit...File Download Complete"
+done
 
-# ls -l all_images | awk '{print $9}' > 
+echo
+echo "PROGRAM FINISHED"
