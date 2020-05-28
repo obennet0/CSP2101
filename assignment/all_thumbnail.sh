f323fe813e0b396041cbf0c3c0007ddac5d16940
obennet0 <obennet0@our.ecu.edu.au>
2020-05-28 09:51:05 +0800

    complete part 1 specific thumbnail component

--- assignment/all_thumbnail.sh
+++ assignment/all_thumbnail.sh
@@ -6,11 +6,6 @@ fi
 
 mkdir all_images/
 
-wget -i urls.txt -P all_images -q
+wget -i url.txt -P all_images -q
 
-ls -l all_images | awk '{print $9}' > imageFiles.txt
-
-while read p; do 
-    echo "$p"
-done < imageFiles.txt
-
+# ls -l all_images | awk '{print $9}' > 
--- assignment/specific_thumbnail.sh
+++ assignment/specific_thumbnail.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
-read -p "Enter the specific thumbnail you would like to download: " thumbnail
+while true; do
+    while true; do
+        read -p "Enter the specific thumbnail of the image you would like to download: " thumbnail
+        echo
+        if [ -f $thumbnail.jpg ]; then
+            echo "File already exists. Please select another image to download"
+            echo
+            continue  
+        elif [[ $thumbnail =~ [DSC0][1-2][0-9]{3} ]]; then
+            echo "You have selected $thumbnail. Searching now."
+            echo
+            break
+        else
+            echo "Invalid input"
+            continue
+        fi
+    done
 
-cat urls.txt | grep $thumbnail > specImage.txt
+    cat url.txt | grep $thumbnail > specific_image.txt
 
-wget -q -i specImage.txt
+    if [ -s specific_image.txt ]; then
+        wget -q -i specific_image.txt
+        rm specific_image.txt
+        break
+    else
+        sleep 1
+        echo "File not found. Check the image thumbnail and try again."
+        rm specific_image.txt
+        echo
+        continue
+    fi
+done 
+
+filesize=$(du -b $thumbnail.jpg | awk '{printf "%3.2f", $1/1000}')
+unit=KB
+
+
+sleep 1
+echo "Downloading $thumbnail, with the filename $thumbnail.jpg, with a filesize of $filesize $unit...File Download Complete"
+echo
+echo "PROGRAM FINISHED"
