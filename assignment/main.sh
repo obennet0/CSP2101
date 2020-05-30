7c13cbe0a18773942601568066b1580a9bff9fcf
obennet0 <obennet0@our.ecu.edu.au>
2020-05-30 13:44:22 +0800

    restructure of main script

--- assignment/main.sh
+++ assignment/main.sh
@@ -13,25 +13,27 @@ run_program='y'
 
 while [[ $run_program == 'y' ]]; do
 
-    echo "Please select from the following options:"
+    echo "Please select one the following options:"
     echo 
-    echo "  To download a SPECIFIC thumbnail----------Enter [1]"
-    echo "  To download ALL thumbnails----------------Enter [2]"
-    echo "  To download a RANGE of images-------------Enter [3]"
-    echo "  To download a number of RANDOM images-----Enter [4]"
-    echo
+
     while true; do 
+
+        echo "  To download a SPECIFIC thumbnail----------Enter [1]"
+        echo "  To download ALL thumbnails----------------Enter [2]"
+        echo "  To download a RANGE of images-------------Enter [3]"
+        echo "  To download a number of RANDOM images-----Enter [4]"
+        echo
         read -p "Enter the option number that matches your requirements: " selection
         echo
+
             if [[ $selection -ge 1 && $selection -le 4 ]]; then
                 echo "You have selected option: $selection"
                 echo
-                sleep 1
                 break
             else
                 echo "INVALID OPTION! Please select from the available options."
                 echo
-                continue
+                
             fi
     done
 
@@ -43,17 +45,15 @@ while [[ $run_program == 'y' ]]; do
                     echo
                     if [ -e $thumbnail.jpg ]; then
                         echo "FILE ALEADY EXISTS! Please select another image to download"
-                        echo
-                        continue 
-                    elif [[ $thumbnail =~ [DSC0][1-2][0-9]{3} ]]; then
+                        echo                         
+                    elif [[ $thumbnail =~ [DSC0][0-9]{4} ]]; then
                         echo "You have selected $thumbnail. Searching now:"
                         echo
                         sleep 1
                         break
                     else
                         echo "INVALID INPUT!"
-                        echo
-                        continue
+                        echo                        
                     fi
                 done
 
@@ -66,14 +66,14 @@ while [[ $run_program == 'y' ]]; do
                     echo "Downloading $thumbnail, with the filename $thumbnail.jpg, with a filesize of $filesize $unit...File Download Complete"
                     rm specific_image.txt
                     break
-                else
-                    echo   
-                    sleep 1    
+                else  
                     echo "FILE NOT FOUND! Check the image thumbnail and try again."
-                    rm specific_image.txt
                     echo
+                    rm specific_image.txt
                 fi
             done
+            echo
+            echo "PROGRAM FINISHED"
             ;;
         '2')
             if [ -d images ]; then
@@ -81,71 +81,82 @@ while [[ $run_program == 'y' ]]; do
             fi
 
             mkdir images
+            
             echo "Downloading into 'images' folder:"
-            echo
-            sleep 1
+            echo   
 
             file=url.txt
 
             for url in $(cat $file); do
-                wget -q $url -P images/
+                wget -q $url -P images
                 file_name=$(echo $url | awk -F / '{print $8}')
                 image_num=$(echo $url | awk -F / '{print $8}' | sed 's/.jpg//')
                 file_size=$(du -b images/$file_name | awk '{printf "%3.2f", $1/1000}')
                 unit=KB
                 echo "Downloading $image_num, with the file name $file_name, with a file size of $file_size $unit...File Download Complete"
             done
+
+            echo
+            echo "PROGRAM FINISHED"
             ;;
         '3')
-            cat url.txt | awk -F / '{print $8}' | sed 's/DSC0//; s/.jpg//' > id_numbers.txt
+            cat url.txt | awk -F / '{print $8}' | sed 's/DSC0//; s/.jpg//' > id_number.txt
 
-            while true; do
-                read -p "Enter a 4 digit number for the STARTING range value: " start
-                if [[ $start -ge 1500 && $start =~ ^[0-9]{4}$ ]]; then
+            while true; do 
+
+                while true; do
+                    read -p "Enter a 4 digit number for the STARTING range value: " start
+                    if [[ $start =~ ^[0-9]{4}$ ]]; then
+                        break
+                    else
+                        echo
+                        echo "INVALID INPUT! Please enter a 4 digit number."
+                        echo                        
+                    fi
+                done
+
+                while true; do
+                    read -p "Enter a 4 digit number for the ENDING range value: " end
+                    echo
+                    if [[ $end -le $start ]]; then
+                        echo "INVALID INPUT! Ending value must be higher that starting value."
+                        echo
+                    elif [[ $end -gt $start && $end =~ ^[0-9]{4}$ ]];then 
+                        break
+                    else
+                        echo "INVALID INPUT! Please enter a 4 digit number."
+                        echo                        
+                    fi
+                done
+
+                file=id_number.txt
+    
+                for id in $(cat $file); do
+                    if [[ $id -ge $start && $id -le $end ]]; then
+                        grep $id url.txt >> range_url.txt
+                    fi
+                done
+
+                if [ -e range_url.txt ]; then
+                    for url in $(cat range_url.txt); do
+                                wget -q $url
+                                file_name=$(echo $url | awk -F / '{print $8}')
+                                image_num=$(echo $url | awk -F / '{print $8}' | sed 's/.jpg//')
+                                file_size=$(du -b $file_name | awk '{printf "%3.2f", $1/1000}')
+                                unit=KB
+                                echo "Downloading $image_num, with the file name $file_name, with a file size of $file_size $unit...File Download Complete"
+                    done 
+                    rm range_url.txt
                     break
                 else
-                    echo
-                    echo "INVALID INPUT!"
-                    echo
-                    continue
+                    echo "Sorry, no files found in that range."                    
                 fi
             done
 
-            while true; do
-                read -p "Enter a 4 digit number for the ENDING range value: " end
-                echo
-                if [[ $end -gt $start && $end =~ ^[0-9]{4}$ ]];then 
-                    break
-                else
-                    echo "INVALID INPUT! Enter a 4 digit number."
-                    echo
-                    continue
-                fi
-            done
+            rm id_number.txt
 
-            file=id_numbers.txt
-
-            for id in $(cat $file); do
-                if [[ $id -ge $start && $id -le $end ]]; then
-                    grep $id url.txt >> range_url.txt
-                fi
-            done
-
-            if [ -e range_url.txt ]; then
-                for url in $(cat range_url.txt); do
-                            wget -q -N $url
-                            file_name=$(echo $url | awk -F / '{print $8}')
-                            image_num=$(echo $url | awk -F / '{print $8}' | sed 's/.jpg//')
-                            file_size=$(du -b $file_name | awk '{printf "%3.2f", $1/1000}')
-                            unit=KB
-                            echo "Downloading $image_num, with the file name $file_name, with a file size of $file_size $unit...File Download Complete"
-                done 
-                rm range_url.txt
-            else
-                echo "Sorry, no files found in that range."
-            fi
-
-            rm id_numbers.txt
+            echo
+            echo "PROGRAM FINISHED"
             ;;
         '4')
             while true; do
@@ -182,6 +193,8 @@ while [[ $run_program == 'y' ]]; do
             done
 
             rm random_img.txt
+            echo
+            echo "PROGRAM FINISHED"
             ;;
         *)
             echo 'Fail'
@@ -192,12 +205,11 @@ while [[ $run_program == 'y' ]]; do
     echo
     read -p 'Run program again? [Enter y for yes] ' run_program
     echo
-    continue
+    
 
 done
 
 rm url.txt
 
-echo "PROGRAM FINISHED"
-
+echo "PROGRAM TERMINATED"
 exit 0
